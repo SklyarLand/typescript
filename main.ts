b4389f9e4b6eb124290546c4ddcbbb13baee4343
Александр Искрицкий <sushu1998@mail.ru>
2022-02-14 09:30:21 +0500

    Выполненнно

--- main.ts
+++ main.ts
@@ -1,13 +1,25 @@
 // возвращает сумму элементов массива
 export function arraySum(arr: number[]): number {
+  return arr.reduce((sum, num) => {
+    return sum += num;
+  }, 0);
 }
 
 // возвращает исходную строку в Title Case
-export function toTitleCase(str: string[]): string {
+// почему то на вход была написана не строка а массив
+export function toTitleCase(str: string): string {
+  let arr: string[] = str.split(" ");
+  arr = arr.map(($str) =>
+    str[0].toUpperCase() + str.slice(1)
+  )
+  return arr.join(" ");
 }
 
 // возвращает Promise, который зарезолвит true, если b больше чем a, и зареджектит, если наоборот
 export function promiseCompare(a: number, b: number): Promise<true> {
+  return new Promise((resolve, reject) => {
+      (a < b) ? resolve(true): reject();
+  });
 }
 
 export async function asyncCompare(): Promise<true> {
@@ -17,10 +29,10 @@ export async function asyncCompare(): Promise<true> {
 
   while (res !== true) {
     try {
-      res = promiseCompare(a, b);
+      res = await promiseCompare(a, b);
     }
     catch (e) {
-      b--;
+      b++;
     }
   }
 
